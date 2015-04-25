4da8bd4714f07a521c9e28ce881500b8eb52e220
it0a <recursive404@gmail.com>
2015-04-25 00:44:38 -0400

    Increasing ver

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mysqldump-extend "0.1.0-SNAPSHOT"
+(defproject mysqldump-extend "0.1.1"
   :description "turns your version control friendly mysqldumps into an extended-insert style script"
   :url "http://github.com/it0a"
   :license {:name "MIT License"}
