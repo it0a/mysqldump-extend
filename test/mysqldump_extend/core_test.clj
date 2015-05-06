20745556ba25df56f2534adfadad0829029bd800
it0a <recursive404@gmail.com>
2015-05-05 20:23:31 -0400

    Adding test

--- test/mysqldump_extend/core_test.clj
+++ test/mysqldump_extend/core_test.clj
@@ -8,8 +8,7 @@
   (testing "Diffs extended-format and result of processing."
     (let [diff-result (diff
      (process-script-file "resources/test_data.sql")
-     (str (slurp "resources/test_data_extended.sql")))]
-      (println (count (nth diff-result 0)))
-      (println (count (nth diff-result 1)))
-      (println (count (nth diff-result 2))))
-    (is (= 0 1))))
+     (process-script-file "resources/test_data_extended.sql"))]
+      (is (= 0
+          (count (first diff-result))
+          (count (second diff-result)))))))
